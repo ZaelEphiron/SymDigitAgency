5cc985943be77bdc0987eb7e84f4d17cd2c880d5
ZaelEphiron <34718106+ZaelEphiron@users.noreply.github.com>
2019-07-01 15:00:24 +0200

    Update Navbar
    Ajout d'une navbar pour naviguer entre les différentes pages qui compose le site

---
+++ CatalogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CatalogController extends AbstractController
+{
+    /**
+     * @Route("/catalog", name="catalog")
+     */
+    public function index()
+    {
+        return $this->render('catalog/catalog.html.twig', [
+            'controller_name' => 'CatalogController',
+            'title' => 'Catalogue de nos services',
+        ]);
+    }
+}
---
+++ ReferencesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReferencesController extends AbstractController
+{
+    /**
+     * @Route("/references", name="references")
+     */
+    public function index()
+    {
+        return $this->render('references/references.html.twig', [
+            'controller_name' => 'ReferencesController',
+            'title' => 'Nos références',
+        ]);
+    }
+}
